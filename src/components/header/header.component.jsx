b62bbaf6b5b0c5d5be6e97e4067a15bbcf4b4dd7
kayvera <mikaylarvr@gmail.com>
2020-06-15 13:37:09 -0700

    updating app to redirect to home if user is signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -34,7 +34,7 @@ const Header = ({ currentUser }) => (
 );
 
 const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUsers,
+  currentUser: state.user.currentUser,
 });
 
 export default connect(mapStateToProps)(Header);
