241d793893b3c8d0170f35da151158b6150ace17
kayvera <mikaylarvr@gmail.com>
2020-06-15 13:19:06 -0700

    added redux and user actions to header

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
+import store from "./redux/store";
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
+
   document.getElementById("root")
 );
