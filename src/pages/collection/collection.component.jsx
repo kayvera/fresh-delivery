dc9f0af854a8868362f5aa2f025054dbfb248147
kayvera <mikaylarvr@gmail.com>
2020-06-17 19:25:52 -0700

    moved our directory and shop data into its own reducer

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { selectCollection } from "../../redux/shop/shop.selector";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
 import "./collection.styles.scss";
 
-const CollectionPage = ({ match }) => {
-  console.log(match.params.collectionId);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2>COLLECTION PAGE</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
